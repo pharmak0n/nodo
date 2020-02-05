4746fc5d91d5d600a64e53c17c4eb0397dd122a6
Antonello <antonello.lipori@gmail.com>
2020-02-05 21:16:51 +0100

    offline, tre percorsi

---
+++ sw.js
@@ -0,0 +1,37 @@
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js').then((registration) => {
+            console.log("Service Worker registration successful: ", registration)
+        }, (err) => {
+            console.log("Registration failed", err)
+        })
+    })
+}
+
+let cache_name = 'mysite-v1'
+
+let urls_to_cache = [
+ 'index.html',
+ 'reparto1.html',
+ 'reparto2.html',
+ 'reparto3.html',
+ 'style.css',
+ 'lib/aframe.min.js',
+ 'lib/aframe-ar.min.js',
+ 'js/custom.js'
+]
+
+self.addEventListener('install', (e) => {
+ e.waitUntil(caches.open(cache_name).then((cache) => {
+  return cache.addAll(urls_to_cache)
+ }) )
+})
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => {
+     if(response)
+      return response
+     else
+      return fetch(e.request)
+    }) )
+})
